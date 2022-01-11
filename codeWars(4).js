0f308c1bad1fc1e685be030dc2ebcc3d3975df1f
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-01-12 00:56:54 +0300

    task4

---
+++ codeWars(4).js
@@ -0,0 +1,7 @@
+/* Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+ */
+
+
+const fakeBin = x => x.replace(/\d/g, d => (d >= 5 ? 1 : 0))
+
+module.exports = fakeBin
