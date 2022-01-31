7b305addd63b6af647fd8739cdea16458f7dc9bc
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-01-31 21:49:45 +0300

    6 kyu

--- Bouncing Balls.js
+++ Bouncing Balls.js
@@ -1,11 +1,17 @@
 function bouncingBall(h,  bounce,  window) {
-   let i = 0;
-   while (h > window) {
-     i++;
-     h *= bounce;
-     if (h > window) {
-       i++;
-     }
-   } 
-   return i;
- }
+  
+  
+  let i = 0;
+  if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) {
+    i = -1;
+  } else{
+    while (h > window) {
+    i++;
+    h *= bounce;
+    if (h > window) 
+      i++;
+    }
+  }
+  
+  return i;
+}
